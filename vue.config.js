aff11c29787dd0f86ea81620a980427128a0346b
eason830 <orz0830@gmail.com>
2022-03-14 20:03:28 +0800

    路徑問題解決

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: './',
+  chainWebpack (config) {
+    config.plugin('html').tap(args => {
+      args[0].title = '麵屋號'
+      return args
+    })
+  }
 }
